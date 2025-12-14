7e1f2f768fd4684d8c758f599b47a9c75fac981f
WissemJderi <asqr43641@gmail.com>
2025-12-14 12:32:22 +0100

    Initialize phonebook-backend with package.json and essential dependencies

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "http://localhost:3001/api/persons";
 
 const getAll = () => {
-  return axios.get(baseUrl).then((response) => response.data);
+  return axios.get(`${baseUrl}`).then((response) => response.data);
 };
 const add = (newPerson) => {
-  return axios.post(baseUrl, newPerson).then((response) => response.data);
+  return axios.post(`${baseUrl}`, newPerson).then((response) => response.data);
 };
 
 const changeNumber = (id, personData) => {
